c44934fc29a801df23298c734e616bbbe5b9bca8
Edgar Benavides <edgarben27@gmail.com>
2022-08-05 10:04:45 -0500

    cors

--- src/config/express.js
+++ src/config/express.js
@@ -1,8 +1,10 @@
 import express from 'express'
+import cors from 'cors'
 
 const expressApp = express()
 
 // Middlewares
+expressApp.use(cors())
 expressApp.use(express.json())
 expressApp.use(express.urlencoded({ extended: true }))
 
